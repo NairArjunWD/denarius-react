2421208ffd5616e7377fcd973429437cd2e120e4
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-06 15:50:25 -0800

    add addstock template

--- frontend/src/Components/layout/AddStock.js
+++ frontend/src/Components/layout/AddStock.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
 
 class AddStock extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: '',
+            company: '',
+            amount: ''
+        }
+    }
+    handleChange = (e) => {
+        this.setState({ [e.currentTarget.name]: e.currentTarget.value })
+    }
 
     componentDidMount() {
         const M = window.M;
@@ -14,13 +26,27 @@ class AddStock extends Component {
         return (
             <div>
 
-                <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Add Stock</a>
+                <a class="waves-effect waves-light btn modal-trigger" href="#modal2">Add Stock</a>
 
 
             <div id="modal2" class="modal">
                 <div class="modal-content">
-                    <h4>Add Stock</h4>
-                    <p>A bunch of text</p>
+                    <h4>Add Stock Card</h4>
+                    
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s6">
+                                    <input id="input_text1" type="text" data-length="10" value={this.state.name}/>
+                                    <label for="input_text1">name</label>
+                                        <input id="input_text2" type="text" data-length="10" value={this.state.company}/>
+                                    <label for="input_text2">company</label>
+                                        <input id="input_text3" type="float" data-length="10" value={this.state.amount}/>
+                                    <label for="input_text3">amount</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
