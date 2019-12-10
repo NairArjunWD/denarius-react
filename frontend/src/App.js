083ec881bd2e6479983f5b81aaa73a0833202d00
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-09 16:42:37 -0800

    Data passing through now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,15 +20,39 @@ state = {
 handleChange = (e) => {
   this.setState({ [e.currentTarget.name]: e.currentTarget.value })
 }
+
+  closeAndAdd = async (stock) => {
+    console.log("WE ARE HERE!!!!");
+    try {
+      const createdStockResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/stocks/`, {
+        method: 'POST',
+        body: JSON.stringify(stock),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const parsedResponse = await createdStockResponse.json();
+      console.log(parsedResponse, ' this is response')
+
+      this.setState({
+        stocks: [...this.state.stocks, parsedResponse.data]
+      })
+    } catch (err) {
+      console.log('error')
+      console.log(err)
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
           <OurModal />
-        <AddStock handleChange={this.handleChange}/>
+        <AddStock handleChange={this.handleChange} closeAndAdd={this.closeAndAdd}/>
         <div>
           <StockContainer />
-          <StockCard card={this.state}/>
+          {/* <StockCard card={this.state}/> */}
         </div>
       </div>
     );
