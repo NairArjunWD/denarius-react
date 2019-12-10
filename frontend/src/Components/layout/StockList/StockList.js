c78318cd794a020d99d6c3f5772d4c2b3e39569d
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-10 11:30:22 -0800

    Fixed a lot of things that are on fire. Id is fixed.

--- frontend/src/Components/layout/StockList/StockList.js
+++ frontend/src/Components/layout/StockList/StockList.js
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Button, Card, Row, Col } from 'react-materialize';
-import StockCard from '../StockCard/StockCard';
+// import StockCard from '../StockCard/StockCard';
+import M from "materialize-css/dist/js/materialize.min.js"
+import { Link } from 'react-router-dom'
 
-function StockList(props) {
-    const stocks = props.stocks.map((stock) => {
+export class StockList extends Component {
+
+    componentDidMount() {
+        // document.addEventListener("DOMContentLoaded", function() {
+        //     const stocks = props.stocks.map((stock)
+        // })
+    }
+    render() {
         return (
-            <StockCard />
-        )
-    });
+            <React.Fragment>
+                <div>
+                    <h1>Stocks</h1>
+                    <Card>
 
-    return
+                        <div class="row">
+                            <div class="col s12 m4">
+                                <div class="card #66bb6a green lighten-1">
+                                    <div class="card-content white-text" key={this.props.stocks.id}>
+                                        
+                                        <span class="card-title">{this.props.card.name}</span>
+                                        
+                                        <ul>
+                                            <li>
+                                                {this.props.card.company}
+                                            </li>
+                                            <li>
+                                                {this.props.card.amount}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="card-action">
+                                        <a href="#"><i class="material-icons">photo_size_select_large
+</i></a>
+                                        <a href="#">Edit</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            </React.Fragment>
+        )
+    };
+
+    // return (
+    //     <div>
+    //         {stocks}
+    //     </div>
+    // )
 }
+
+export default StockList
