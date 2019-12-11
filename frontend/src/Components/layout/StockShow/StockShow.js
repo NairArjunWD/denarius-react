84a1f56d5ac68a240cf7583450a0ebf0be74a362
Arjun Nair <nair.arjun47@yahoo.com>
2019-12-10 16:08:29 -0800

    add getStock async to stockshow

--- frontend/src/Components/layout/StockShow/StockShow.js
+++ frontend/src/Components/layout/StockShow/StockShow.js
@@ -11,6 +11,19 @@ class StockShow extends Component {
     }
 
     getStock = async () => {
-        const stockId = this.props.match.params.id
+        const stockId = this.props.match.params.id;
+        try {
+            const stocks = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/stocks`);
+            const parsedStocks = await stocks.json();
+            console.log(parsedStocks);
+
+            this.setState({
+                stocks: parsedStocks.data.stocks
+
+            });
+            console.log(stocks)
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
